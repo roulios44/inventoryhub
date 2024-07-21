cbd9612897299af0b3971acd0b74951f96d87bf6
Jules CROIZIER <jules.croizier@gmail.com>
2024-07-21 11:48:18 +0200

    adding dtos

---
+++ src/main/java/com/inventoryhub/inventoryhub/dtos/StateDTO.java
@@ -0,0 +1,37 @@
+
+package com.inventoryhub.inventoryhub.dtos;
+
+import lombok.Data;
+
+@Data
+public class StateDTO {
+
+    private Long id;
+    private String name;
+
+    // Constructors, getters and setters
+
+    public StateDTO() {
+    }
+
+    public StateDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
